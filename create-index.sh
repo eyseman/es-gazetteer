3eed92ca3a924bd16893bb07e801722ac151dbce
Thomas J. Lampoltshammer <thomas.lampoltshammer@gmail.com>
2020-03-23 19:10:30 +0100

    Update create-index.sh

--- create-index.sh
+++ create-index.sh
@@ -1,5 +1,5 @@
 echo "Starting Docker container and data volume..."
-sudo docker run -d -p 127.0.0.1:9200:9200 -v $PWD/geonames_index/:/usr/share/elasticsearch/data elasticsearch:7.6.1
+sudo docker run -d -p 127.0.0.1:9200:9200 -v $PWD/geonames_index/:/usr/share/elasticsearch/data --name elasticsearch elasticsearch:7.6.1
 
 echo "Downloading Geonames gazetteer..."
 wget http://download.geonames.org/export/dump/allCountries.zip
